76fd0a55fe2e758d9c71c5cbcaec3a6af8af1949
Kapil <ky8050989@gmail.com>
2024-07-31 16:32:59 +0530

    files were modified

--- prefixevaluation.cpp
+++ prefixevaluation.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int prefixevaluation(string s)
 {
     stack<int> st;
-    for (int i = s.length() - 1; i >= 0; i--)
+    for (int i = s.length() - 1; i >= 0; i--) // it starts from backward as the prefix expressions are evaluated from the backside.
     {
-        if (s[i] >= '0' && s[i] <= '9')
+        if (s[i] >= '0' && s[i] <= '9') // This line checks if the c urrent character s[i] is a digit (between '0' and '9').
         {
-            st.push(s[i] - '0');
+            st.push(s[i] - '0'); // here it pushes the operand into the stack.
         }
-        else
+        else // If the character s[i] is an operator, pop the top two elements from the stack. These elements are the operands for the operator.
         {
-            int op1 = st.top();
-            st.pop();
+            int op1 = st.top(); // This line pops the top operand from the stack and stores it in op1.
+            st.pop();           // This line pops the top operand from the stack (already stored in op1).
             int op2 = st.top();
             st.pop();
             switch (s[i])
--- queue.cpp
+++ queue.cpp
@@ -5,28 +5,28 @@ using namespace std;
 class queue
 {
 public:
-    int *arr;
+    int *arr; // pointer to an array that will store the queue elements.
     int front;
     int end;
-#define n 6
+#define n 6 // Defines a constant n with a value of 6, representing the maximum size of the queue.
     queue()
     {
         arr = new int[n];
-        front = -1;
+        front = -1; // Initializes front to -1, indicating that the queue is initially empty.
         end = -1;
     }
     void push(int x)
     {
-        if (end == n - 1)
+        if (end == n - 1) // The last position in the array is n-1. So, when end equals n-1, it means there is no more space to add new elements, and the queue is full.
         {
             cout << "queue is Full" << endl;
             return;
         }
         end++;
         arr[end] = x;
-        if (front == -1)
-        {
-            front++;
+        if (front == -1) // Checks if the queue was previously empty.
+        {                // When you add the first element to the queue, you increment end to 0 and set the first element in the array. However, since front is still -1, it needs to be updated to point to the first element as well.
+            front++;     // sets front to 0 , if it was empty.
         }
     }
     ////////////////////////////////////////////////////////////////////
@@ -34,6 +34,10 @@ public:
     void pop()
     {
         if (front == -1 || front > end)
+        // why front > end :
+        // Each time an element is popped from the queue, front is incremented. , it will point to next element as the first element is popped.
+        // If all elements are popped, front will eventually become greater than end.
+        // For example, if end is 2 and we pop three elements, front will be 3.
         {
             cout << "no elements present." << endl;
             return;
